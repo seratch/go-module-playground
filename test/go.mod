a51caacea26db935d2f93da569b466555f4eb9f4
Kazuhiro Sera <seratch@gmail.com>
2020-09-05 19:10:25 +0900

    v4 test

--- test/go.mod
+++ test/go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/seratch/go-module-playground/v2 v2.0.1 // indirect
 	github.com/seratch/go-module-playground/v3 v3.0.0 // indirect
+	github.com/seratch/go-module-playground/v4 v4.0.0 // indirect
 )
--- test/main.go
+++ test/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	hello2 "github.com/seratch/go-module-playground/v2"
 	hello3 "github.com/seratch/go-module-playground/v3"
+	hello4 "github.com/seratch/go-module-playground/v4"
 )
 
 func main() {
+	fmt.Println(hello4.Version())
 	fmt.Println(hello3.Version())
 	fmt.Println(hello3.Hello())
 	fmt.Println(hello2.Version())
